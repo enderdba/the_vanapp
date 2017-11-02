b85676fc6ba709c0d42456ff6d8d928b81e19dcb
enderdba <ender@phanta-soft.com>
2017-11-02 13:59:28 -0400

    ok

---
+++ www/js/maps.js
@@ -0,0 +1,77 @@
+function showMap(dmap, smap, latitude, longitude) {
+    var container = dmap.get(0);
+    var map = new google.maps.Map(container, {
+        center: { lat: latitude, lng: longitude },
+        zoom: 10,
+        fullscreenControl: false,
+        disableDefaultUI: true
+    });
+
+    var marker = new google.maps.Marker({
+        map: map,
+        draggable: true
+    });
+
+    var volverBtn = document.createElement("a");
+    volverBtn.innerHTML = "VOLVER";
+    volverBtn.className = "btn mapControls";
+    volverBtn.addEventListener('click', toggleMap);
+    var ubiqBtn = document.createElement("a");
+    ubiqBtn.addEventListener('click', function () {
+        getPosition(marker, map);
+    });
+    ubiqBtn.innerHTML = "<i class='material-icons'>gps_fixed</i>";
+    ubiqBtn.className = "btn mapControls";
+    map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(volverBtn);
+    map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(ubiqBtn);
+
+
+    google.maps.event.addListener(map, 'click', function (event) {
+        placeMarker(event.latLng, marker, map);
+    });
+    smap.click(function () {
+        toggleMap(marker, map);
+        alreadyAuto = true;
+    });
+}
+
+function placeMarker(location, marker, map) {
+    marker.setPosition(location);
+    map.panTo(location)
+        .setZoom(18);
+    console.log(location.lat(), ",", location.lng());
+    loadStaticMap(location.lat(), location.lng());
+}
+
+function loadStaticMap(lat, lng, smap) {
+    smap
+        .show()
+        .attr("src",
+        "https://maps.googleapis.com/maps/api/staticmap?zoom=18"
+        + "&center=" + lat + "," + lng + "&size=" + smap.width().toFixed(0) + "x100&zoom=9&maptype=roadmap&markers=color:red%7Clabel:A%7C" + lat + "," + lng + "&key=AIzaSyBvzD6w4CpxJu_giqVDSJ43pBN7TTcLMfQ")
+        .css({
+            "margin-top": "0px"
+        })
+}
+
+function getPosition(marker, map) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        placeMarker(myLatlng, marker, map);
+    });
+}
+
+function toggleMap(marker, map) {
+    $(".gmnoprint").hide();
+    $('#map img.gm-fullscreen-control').parent("button").trigger('click');
+    if (!alreadyAuto) {
+        getPosition(marker, map);
+    }
+}
+
+function triggerMap(smap) {
+    smap.css({
+        "width": "100%",
+        "height": "100%"
+    })
+}
--- www/js/splash.js
+++ www/js/splash.js
@@ -1,7 +1,6 @@
 //flags
 var alreadyAuto = false;
-
-if (typeof cordova === null) {
+if (window.cordova != undefined) {
   document.addEventListener("deviceready", onDeviceReady, false);
 } else {
   $(document).ready(function () {
@@ -9,17 +8,13 @@ if (typeof cordova === null) {
   })
 }
 
-function triggerMap() {
-  $("#map").css({
-    "width": "100%",
-    "height": "100%"
-  })
-}
-
 function onDeviceReady() {
-  $(".map-salvaje").hide();
+  var smap = $(".map-salvaje");
+  smap.hide();
+  var dmap = $("#map");
   navigator.geolocation.getCurrentPosition(function (position) {
-    loadStaticMap(position.coords.latitude, position.coords.longitude);
+    loadStaticMap(position.coords.latitude, position.coords.longitude, smap);
+    showMap(smap, dmap, position.coords.latitude, position.coords.longitude);
   });
   $(".logo").transition({
     "opacity": "1.0",
@@ -32,7 +27,6 @@ function onDeviceReady() {
         "opacity": "0"
       }, 1000, function () {
         $(".logo").hide();
-
         $(".vform").transition({
           "opacity": "1.0"
         }, 1000);
@@ -48,7 +42,7 @@ function onDeviceReady() {
 
   $(".slide").click(function (e) {
     e.preventDefault();
-    if (typeof cordova !== null) {
+    if (window.cordova != undefined) {
       window.plugins.nativepagetransitions.slide({
         // the defaults for direction, duration, etc are all fine
         "href": "boi.html"
@@ -64,70 +58,6 @@ function onDeviceReady() {
 });*/
 }
 
-function showMap() {
-  var container = $('#map').get(0);
-  var map = new google.maps.Map(container, {
-    center: { lat: 10.578474, lng: -71.612383 },
-    zoom: 10,
-    fullscreenControl: false,
-    disableDefaultUI: true
-  });
-
-  var marker = new google.maps.Marker({
-    map: map,
-    draggable: true
-  });
-
-  var volverBtn = document.createElement("a");
-  volverBtn.innerHTML = "VOLVER";
-  volverBtn.className = "btn mapControls";
-  volverBtn.addEventListener('click', toggleMap);
-  var ubiqBtn = document.createElement("a");
-  ubiqBtn.addEventListener('click', function () {
-    getPosition(marker, map);
-  });
-  ubiqBtn.innerHTML = "<i class='material-icons'>gps_fixed</i>";
-  ubiqBtn.className = "btn mapControls";
-  map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(volverBtn);
-  map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(ubiqBtn);
 
 
-  google.maps.event.addListener(map, 'click', function (event) {
-    placeMarker(event.latLng, marker, map);
-  });
-  $('.map-salvaje').click(function () {
-    toggleMap(marker, map);
-    alreadyAuto = true;
-  });
-}
 
-function placeMarker(location, marker, map) {
-  marker.setPosition(location);
-  map.panTo(location);
-  map.setZoom(18);
-  console.log(location.lat(), ",", location.lng());
-  loadStaticMap(location.lat(), location.lng());
-}
-
-function loadStaticMap(lat, lng) {
-  $('.map-salvaje').show();
-  $('.map-salvaje').attr("src", "https://maps.googleapis.com/maps/api/staticmap?zoom=18&center=" + lat + "," + lng + "&size=" + $(".map-salvaje").width().toFixed(0) + "x100&zoom=9&maptype=roadmap&markers=color:red%7Clabel:A%7C" + lat + "," + lng + "&key=AIzaSyBvzD6w4CpxJu_giqVDSJ43pBN7TTcLMfQ");
-  $('.map-salvaje').css({
-    "margin-top": "0px"
-  })
-}
-
-function getPosition(marker, map) {
-  navigator.geolocation.getCurrentPosition(function (position) {
-    myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    placeMarker(myLatlng, marker, map);
-  });
-}
-
-function toggleMap(marker, map) {
-  $(".gmnoprint").hide();
-  $('#map img.gm-fullscreen-control').parent("button").trigger('click');
-  if (!alreadyAuto) {
-    getPosition(marker, map);
-  }
-}
